5f087fc6f07ff15a60036e9e0a0a9bcfc15e104f
iOnilec <c.onilec.0t@gmail.com>
2025-07-21 14:39:44 -0300

    feat: python/var.py - variable, types, input, string, concatenation in python

---
+++ python/var.py
@@ -0,0 +1,36 @@
+# Manipulação de váriaveis e constantes
+
+# int
+number_int_1 = 23
+number_int_2 = -90
+
+# Printar váriaveis separadamente
+print(number_int_1)
+print(number_int_2)
+
+# Printar váriaveis concatenadas
+print(number_int_1, number_int_2, 'Oi, Mundo.')
+
+# Strings e chars
+word_str_1 = 'linguagem de programação'
+word_str_2 = 'chamada Python'
+
+print('Estou aprendendo uma', word_str_1, word_str_2)
+
+# Input (entrada do usuário)
+age = input('Qual a sua idade? ')
+
+print('Você tem', age, 'anos de idade')
+
+name = input('Qual é o seu nome? ')
+
+print('Seu nome é', name)
+
+# Adicionar a tipagem no input()
+age_type = int(input('Qual a sua idade? '))
+
+print('Sua idade é', age_type)
+
+name_type = str(input('Qual é o seu nome? '))
+
+print('Seu nome é', name_type)
