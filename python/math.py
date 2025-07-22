db62583fd8297f27a760af9caae72eaedd4f69fd
iOnilec <c.onilec.0t@gmail.com>
2025-07-22 11:25:35 -0300

    feat: math operator and import math

---
+++ python/math.py
@@ -0,0 +1,31 @@
+# Operações matemáticas
+
+# Soma
+a = 453
+b = 234
+print(a + b)
+
+# Subtração
+print(a - b)
+
+# Divisão
+print(a / b)
+
+# Multiplicação
+print(a * b)
+
+# Resto da divisão
+print(10 % 3)
+
+# Elevar
+print(5**4)
+
+import math
+
+print(math.sqrt(81))
+
+# Arredondar
+c = 92874
+d = 928374
+e = c / d
+print(round(e, 2))
