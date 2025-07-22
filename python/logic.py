daec9f1b551e5b239662dc2c2ad71f29f4500a6a
iOnilec <c.onilec.0t@gmail.com>
2025-07-22 12:02:09 -0300

    feat: logic operators and relations operator

---
+++ python/logic.py
@@ -0,0 +1,38 @@
+# Operadores lógicos
+
+a = True
+b = False
+
+# São iguais?
+print(a, b)
+print(a and b)
+print(a & b)
+print("'A' e 'B' são iguais é", a & b)
+
+# Ou são iguais?
+print(a or b)
+print(a | b)
+print("'A' e 'B' são iguais é", a | b)
+
+# Não é True ou False?
+print(not a)
+print(not b)
+
+
+# Operadores relacionais
+
+c = 45
+d = 46
+
+# C é maior que D?
+print(c > d)
+# C é igual a D?
+print(c == d)
+# C é maior ou igual a D?
+print(c >= d)
+# C é menor que D?
+print(c < d)
+# C é menor ou igual a D?
+print(c <= d)
+# C é diferente de D?
+print(c != d)
